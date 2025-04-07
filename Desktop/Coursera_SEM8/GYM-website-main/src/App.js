b770808aee02f4828bdc566902f8d3f050437969
Priyansh218 <priyanshdahake@gmail.com>
2025-04-07 22:48:09 +0530

    Latest Change

---
+++ Desktop/Coursera_SEM8/GYM-website-main/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import OG from './OG';
+
+function App() {
+  return (
+    <div className="App">
+      <OG/>
+    </div>
+  );
+}
+
+export default App;
